75f685e48789720b6b80b4f668e2336aba84af0e
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2020-11-12 20:26:29 +1300

    Create nodes in lung scaffold - two different cut angles

--- src/scaffoldmaker/meshtypes/meshtype_3d_left_lung_Arti.py
+++ src/scaffoldmaker/meshtypes/meshtype_3d_left_lung_Arti.py
@@ -33,6 +33,9 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
             'Number of element (base length)': 4,
             # 'Number of element (height)': 4,
             'Use cross derivatives': False,
+            'Cut from the corner': False,
+            'Complete': False,
+            'Depth': 0,
             'Refine': False,
             'Refine Element in Width': 1,
             'Refine Element in Length': 1,
@@ -50,6 +53,9 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
             'Number of element (base length)',
             # 'Number of element (height)',
             'Use cross derivatives',
+            'Cut from the corner',
+            'Complete',
+            'Depth',
             'Refine',
             'Refine Element in Width',
             'Refine Element in Length',
@@ -70,6 +76,8 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
             options['Number of element (base width)'] = 1
         if (options['Number of element (base length)'] < 1):
             options['Number of element (base length)'] = 1
+        if (options['Depth'] < 0):
+            options['Depth'] = 0
         # if (options['Number of element (base height)'] < 1):
         #     options['Number of element (base height)'] = 1
 
@@ -87,6 +95,9 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
         elementsCountAlong = options['Number of element (base length)']
         elementsCountIn = options['Number of element (base width)']
         useCrossDerivatives = options['Use cross derivatives']
+        cutAngle = options['Cut from the corner']
+        cutGradient = options['Depth']
+        complete = options['Complete']
 
         fm = region.getFieldmodule()
         fm.beginChange()
@@ -116,26 +127,51 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
 
 
         #create nodes
-        nodeIdentifier = 1
-        x = [0.0, 0.0, 0.0]
-        dx_ds1 = [0.0, 0.0, 0.0]
-        dx_ds2 = [0.0, 0.0, 0.0]
-        dx_ds3 = [0.0, 0.0, 0.0]
-        zero = [0.0, 0.0, 0.0]
-        bodyLength = length - width
-        stepLength = length/elementsCountAlong
-        stepWidth = width/elementsCountIn
-        # since #elements in height is proportional to #elements in length
-        stepHeight = height/elementsCountAlong
+        if cutAngle != True:
+            nodeIdentifier = 1
+            x = [0.0, 0.0, 0.0]
+            dx_ds1 = [0.0, 0.0, 0.0]
+            dx_ds2 = [0.0, 0.0, 0.0]
+            dx_ds3 = [0.0, 0.0, 0.0]
+            zero = [0.0, 0.0, 0.0]
+            bodyLength = length - width
+            stepLength = length/elementsCountAlong
+            stepWidth = width/elementsCountIn
+            # since #elements in height is proportional to #elements in length
+            stepHeight = height/elementsCountAlong
 
-        for n3 in range(elementsCountAlong+2):
-            x[2] = n3 * stepHeight
-            for n2 in range(elementsCountIn+1):
-                x[1] = n2 * stepWidth
-                for n1 in range(elementsCountAlong+1):
-                    x[0] = (n3 * stepLength + n1 * stepLength)
+            for n3 in range(elementsCountAlong+2):
+                x[2] = n3 * stepHeight
+                for n2 in range(elementsCountIn+1):
+                    x[1] = n2 * stepWidth
+                    for n1 in range(elementsCountAlong+1):
+                        x[0] = (n3 * stepLength + n1 * stepLength)
 
-                    if x[0] <= length:
+                        if x[0] <= length:
+                            node = nodes.createNode(nodeIdentifier, nodetemplate)
+                            cache.setNode(node)
+
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+
+                            if useCrossDerivatives:
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+
+                            nodeIdentifier = nodeIdentifier + 1
+
+                    if n2 > 0 and n2 < elementsCountIn:
+                        yCircle = (width / 2) - x[1]
+                        xCircle = math.sqrt((width/2)**2 - yCircle**2)
+
+                        x[0] = rfactor * xCircle + length
+                        print("n2: ", n2)
+                        print("bodylength: ", bodyLength)
+                        print("node: ", nodeIdentifier, "||", x)
                         node = nodes.createNode(nodeIdentifier, nodetemplate)
                         cache.setNode(node)
 
@@ -151,30 +187,77 @@ class MeshType_3d_left_lung_Arti(Scaffold_base):
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
                         nodeIdentifier = nodeIdentifier + 1
+        else:
+            nodeIdentifier = 1
+            x = [0.0, 0.0, 0.0]
+            dx_ds1 = [0.0, 0.0, 0.0]
+            dx_ds2 = [0.0, 0.0, 0.0]
+            dx_ds3 = [0.0, 0.0, 0.0]
+            zero = [0.0, 0.0, 0.0]
+            bodyLength = length - width
+            stepLength = length / elementsCountAlong
+            stepWidth = width / elementsCountIn
+            # since #elements in height is proportional to #elements in length
+            stepHeight = height / elementsCountAlong
 
-                if n2 > 0 and n2 < elementsCountIn:
-                    yCircle = (width / 2) - x[1]
-                    xCircle = math.sqrt((width/2)**2 - yCircle**2)
+            n3 =0
+            count = 0
 
-                    x[0] = rfactor * xCircle + length
-                    print("n2: ", n2)
-                    print("bodylength: ", bodyLength)
-                    print("node: ", nodeIdentifier, "||", x)
-                    node = nodes.createNode(nodeIdentifier, nodetemplate)
-                    cache.setNode(node)
+            while count != 2:
+                count = 0
+                x[2] = n3 * stepHeight
+                for n2 in range(elementsCountIn + 1):
+                    x[1] = n2 * stepWidth
+                    for n1 in range(elementsCountAlong + 1):
+                        if n3 + cutGradient <= n2 + n1:
+                            x[0] = n1 * stepLength
+                            if x[0] <= length:
+                                node = nodes.createNode(nodeIdentifier, nodetemplate)
+                                cache.setNode(node)
 
-                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
-                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
-                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
-                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
 
-                    if useCrossDerivatives:
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                                if useCrossDerivatives:
+                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
-                    nodeIdentifier = nodeIdentifier + 1
+                                # print("node: ", nodeIdentifier, "||", x)
+                                count = count + 1
+                                nodeIdentifier = nodeIdentifier + 1
+
+                    if n2 > 0 and n2 < elementsCountIn and n3 + cutGradient <= n2 + n1 +1:
+                        yCircle = (width / 2) - x[1]
+                        xCircle = math.sqrt((width / 2) ** 2 - yCircle ** 2)
+
+                        x[0] = rfactor * xCircle + length
+                        node = nodes.createNode(nodeIdentifier, nodetemplate)
+                        cache.setNode(node)
+
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+
+                        if useCrossDerivatives:
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+
+                        # print("node: ", nodeIdentifier, "||", x)
+                        count = count + 1
+                        nodeIdentifier = nodeIdentifier + 1
+
+                print("n3: ", n3)
+                print("count: ", count)
+
+
+                n3 += 1
 
 
 
