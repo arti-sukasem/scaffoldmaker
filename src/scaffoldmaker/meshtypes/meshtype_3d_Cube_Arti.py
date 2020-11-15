77df406278dcfcf844ad894bf2b17f8ad9b46f69
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2020-11-16 11:27:04 +1300

    Revert "Add wedges on cube"
    This reverts commit a9b57b4d52c2650f4ede30ed7f46b62fb1efd8fa.

--- src/scaffoldmaker/meshtypes/meshtype_3d_Cube_Arti.py
+++ src/scaffoldmaker/meshtypes/meshtype_3d_Cube_Arti.py
@@ -26,10 +26,8 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
             'Number of elements 1' : 1,
             'Number of elements 2' : 1,
             'Number of elements 3': 1,
-            'Wedge 1': False,
-            'Wedge 2': False,
+            'Wedge': False,
             'Use cross derivatives' : False,
-            'Refine': False,
             'Refine Element 1': 1,
             'Refine Element 2': 1,
             'Refine Element 3': 1,
@@ -42,10 +40,8 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
             'Number of elements 1',
             'Number of elements 2',
             'Number of elements 3',
-            'Wedge 1',
-            'Wedge 2',
+            'Wedge',
             'Use cross derivatives',
-            'Refine',
             'Refine Element 1',
             'Refine Element 2',
             'Refine Element 3',
@@ -65,7 +61,6 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
             options['Refine Element 2'] = 1
         if (options['Refine Element 3'] < 1) :
             options['Refine Element 3'] = 1
-
     @classmethod
     def generateBaseMesh(cls, region, options):
         """
@@ -77,8 +72,7 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
         elementsCount1 = options['Number of elements 1']
         elementsCount2 = options['Number of elements 2']
         elementsCount3 = options['Number of elements 3']
-        createWedge1 = options['Wedge 1']
-        createWedge2 = options['Wedge 2']
+        createWedge = options['Wedge']
         useCrossDerivatives = options['Use cross derivatives']
 
         fm = region.getFieldmodule()
@@ -97,22 +91,9 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
             nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D2_DS2DS3, 1)
             nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1)
 
-
-
         mesh = fm.findMeshByDimension(3)
         tricubichermite = eftfactory_tricubichermite(mesh, useCrossDerivatives)
         eft = tricubichermite.createEftBasic()
-        if createWedge1:
-            eft1 = tricubichermite.createEftWedgeXi1Zero()
-            elementtemplate1 = mesh.createElementtemplate()
-            elementtemplate1.setElementShapeType(Element.SHAPE_TYPE_CUBE)
-            elementtemplate1.defineField(coordinates, -1, eft1)
-        if createWedge2:
-            eft2 = tricubichermite.createEftWedgeXi1One()
-            elementtemplate2 = mesh.createElementtemplate()
-            elementtemplate2.setElementShapeType(Element.SHAPE_TYPE_CUBE)
-            elementtemplate2.defineField(coordinates, -1, eft2)
-
         elementtemplate = mesh.createElementtemplate()
         elementtemplate.setElementShapeType(Element.SHAPE_TYPE_CUBE)
         elementtemplate.defineField(coordinates, -1, eft)
@@ -143,17 +124,6 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
-                    if createWedge1:
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, zero)
-                    if createWedge2:
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
-                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, zero)
-
                     nodeIdentifier = nodeIdentifier + 1
 
         #create element
@@ -163,31 +133,13 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
         for e3 in range(elementsCount3):
             for e2 in range(elementsCount2):
                 for e1 in range(elementsCount1):
-                    if createWedge1 == True and e1<1 and e3 == elementsCount3-1:
-                        element1 = mesh.createElement(elementIdentifier, elementtemplate1)
-                        bni = e3 * no3 + e2 * no2 + e1 + 1
-                        nodeIdentifiers = [bni, bni + 1, bni + no2, bni + no2 + 1,
-                                            bni + no3 + 1, bni + no2 + no3 + 1]
-                        result = element1.setNodesByIdentifier(eft1, nodeIdentifiers)
-                        print("elementidentifier: ", elementIdentifier, " || ", nodeIdentifiers)
-                        elementIdentifier = elementIdentifier + 1
-                    elif createWedge2 == True and e1 == elementsCount1-1 and e3 == elementsCount3-1:
-                        element2 = mesh.createElement(elementIdentifier, elementtemplate2)
-                        bni = e3 * no3 + e2 * no2 + e1 + 1
-                        nodeIdentifiers = [bni, bni + 1, bni + no2, bni + no2 + 1,
-                                           bni + no3, bni + no2 + no3]
-                        result = element2.setNodesByIdentifier(eft2, nodeIdentifiers)
-                        print("elementidentifier: ", elementIdentifier, " || ", nodeIdentifiers)
-                        elementIdentifier = elementIdentifier + 1
-                    else:
-                        element = mesh.createElement(elementIdentifier, elementtemplate)
-                        bni = e3 * no3 + e2 * no2 + e1 + 1
-                        nodeIdentifiers = [bni, bni + 1, bni + no2, bni + no2 + 1,
-                                           bni + no3, bni + no3 + 1, bni + no2 + no3, bni + no2 + no3 + 1]
-                        result = element.setNodesByIdentifier(eft, nodeIdentifiers)
-                        print("elementidentifier: ", elementIdentifier, " || ", nodeIdentifiers)
-                        elementIdentifier = elementIdentifier + 1
-
+                    element = mesh.createElement(elementIdentifier, elementtemplate)
+                    bni = e3*no3 + e2*no2 + e1 + 1
+                    nodeIdentifiers = [bni, bni + 1, bni + no2, bni + no2 + 1,
+                                       bni + no3, bni + no3 + 1, bni + no2 + no3, bni + no2 + no3 + 1]
+                    element.setNodeByIdentifier(eft, nodeIdentifiers)
+                    print("elementidentifier: ", elementIdentifier, " || ", nodeIdentifiers)
+                    elementIdentifier = elementIdentifier + 1
 
         fm.endChange()
         return []
@@ -199,9 +151,9 @@ class MeshType_3d_Cube_Arti(Scaffold_base):
         :param meshrefinement: MeshRefinement, which knows source and target region.
         :param options: Dict containing options. See getDefaultOptions().
         """
-        refineElementsCount1 = options['Refine Element 1']
-        refineElementsCount2 = options['Refine Element 2']
-        refineElementsCount3 = options['Refine Element 3']
+        refineElementsCount1 = options['Refine number of elements around']
+        refineElementsCount2 = options['Refine number of elements along']
+        refineElementsCount3 = options['Refine number of elements through wall']
 
         meshrefinement.refineAllElementsCubeStandard3d(refineElementsCount1, refineElementsCount2,
                                                        refineElementsCount3)
