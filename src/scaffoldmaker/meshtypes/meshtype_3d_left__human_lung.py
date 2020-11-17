87a4aa48eae644598126469ad38e3d5d2703b268
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2020-11-17 16:02:20 +1300

    Change dimensions (i.e. x & y) and coding style according to Richard's suggestion

--- src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
+++ src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
@@ -15,7 +15,7 @@ from scaffoldmaker.utils.meshrefinement import MeshRefinement
 
 class MeshType_3d_left_human_lung(Scaffold_base):
     '''
-    classdocs
+    3D lung scaffold for human
     '''
 
     @staticmethod
@@ -28,15 +28,14 @@ class MeshType_3d_left_human_lung(Scaffold_base):
             'Width': 0.25,
             'Length': 1.0,
             'Height': 1.0,
-            'Radius factor': 0.5,
-            'Number of element (base width)': 2,
-            'Number of element (base length)': 4,
-            'Number of element (height)': 3,
+            'Number of element 2': 4,
+            'Number of element 1': 2,
+            'Number of element 3': 3,
             'Use cross derivatives': False,
             'Refine': False,
-            'Refine Element in Width': 1,
-            'Refine Element in Length': 1,
-            'Refine Element in Height': 1,
+            'Refine Element 1': 1,
+            'Refine Element 2': 1,
+            'Refine Element 3': 1,
         }
 
     @staticmethod
@@ -45,33 +44,28 @@ class MeshType_3d_left_human_lung(Scaffold_base):
             'Width',
             'Length',
             'Height',
-            'Radius factor',
-            'Number of element (base width)',
-            'Number of element (base length)',
-            'Number of element (height)',
+            'Number of element 1',
+            'Number of element 2',
+            'Number of element 3',
             'Use cross derivatives',
             'Refine',
-            'Refine Element in Width',
-            'Refine Element in Length',
-            'Refine Element in Height'
+            'Refine Element 1',
+            'Refine Element 2',
+            'Refine Element 3'
         ]
 
     @staticmethod
     def checkOptions(options):
-        if (options['Width'] < 0):
-            options['Width'] = 1.0
-        if (options['Length'] < 0):
-            options['Length'] = 1.0
-        if (options['Height'] < 0):
-            options['Height'] = 1.0
-        if (options['Radius factor'] < 0):
-            options['Radius factor'] = 1.0
-        if (options['Number of element (base width)'] < 2):
-            options['Number of element (base width)'] = 2
-        if (options['Number of element (base length)'] < 4):
-            options['Number of element (base length)'] = 4
-        if (options['Number of element (height)'] < 3):
-            options['Number of element (height)'] = 3
+        for key in ['Width', 'Length', 'Height', 'Refine Element 1', 'Refine Element 2', 'Refine Element 3']:
+            if options[key] < 1:
+                options[key] = 1
+
+        if (options['Number of element 2'] < 4):
+            options['Number of element 2'] = 4
+        if (options['Number of element 1'] < 2):
+            options['Number of element 1'] = 2
+        if (options['Number of element 3'] < 3):
+            options['Number of element 3'] = 3
 
     @classmethod
     def generateBaseMesh(cls, region, options):
@@ -83,10 +77,9 @@ class MeshType_3d_left_human_lung(Scaffold_base):
         width = options['Width']
         length = options['Length']
         height = options['Height']
-        rfactor = options['Radius factor']
-        elementsCountAlong = options['Number of element (base length)']
-        elementsCountIn = options['Number of element (base width)']
-        elementsCountUp = options['Number of element (height)']
+        elementsCount1 = options['Number of element 1']
+        elementsCount2 = options['Number of element 2']
+        elementsCount3 = options['Number of element 3']
         useCrossDerivatives = options['Use cross derivatives']
 
         fm = region.getFieldmodule()
@@ -122,80 +115,119 @@ class MeshType_3d_left_human_lung(Scaffold_base):
         dx_ds2 = [0.0, 0.0, 0.0]
         dx_ds3 = [0.0, 0.0, 0.0]
         zero = [0.0, 0.0, 0.0]
-        stepLength = length/elementsCountAlong
-        stepWidth = width/elementsCountIn
-        stepHeight = height/(elementsCountUp)
-        #update the loop
-        update = 0
+        stepLength = length/elementsCount1
+        stepWidth = width/elementsCount2
+        stepHeight = height/elementsCount3
+        lNodeIds = []   # Node index list
+        update = 0  # Update the loop to eliminate for the slope
 
-        for n3 in range(elementsCountUp+1):
+
+        for n3 in range(elementsCount3+1):
+            lNodeIds.append([])
             x[2] = n3 * stepHeight
-            for n2 in range(elementsCountIn+1):
+            for n2 in range(elementsCount2+1):
+                lNodeIds[n3].append([])
                 x[1] = n2 * stepWidth
-                for n1 in range(elementsCountAlong+1):
-                    # need to find a better way to do this in the future (if and elif)
-                    if (n1 == 0 or n1 == elementsCountAlong - update) and (n2 == 0 or n2 == elementsCountIn) and (n3 <1):
+                for n1 in range(elementsCount1+1):
+                    lNodeIds[n3][n2].append(None)
+
+                    if (n1 == 0 or n1 == elementsCount1) and (n2 == 0 or n2 == elementsCount2):
                         continue
-                    elif (n1 == elementsCountAlong - update and (n2 ==0 or n2 == elementsCountIn)):
+                    elif (n2 > elementsCount2 - update):
                         continue
-                    else:
-                        # extra nodes for triangle element in the middle without affecting other nodes
-                        # at (n1 + 1/2, n2, n3)
-                        if n1 == 1 and n3 == 1:
-                            x[0] = (0.5 * n3 * stepLength + n1 * stepLength)
+
+                    # extra nodes for triangle element in the middle without affecting other nodes
+                    # at (n1 + 1/2, n2, n3)
+                    if n2 == elementsCount2-1 and n3 == 1 and n1 == 0:
+                        x[1] = n2*stepWidth - stepWidth/2
+                        for n12 in range(elementsCount1+1):
+                            x[0] = n12 * stepLength
 
                             node = nodes.createNode(nodeIdentifier, nodetemplate)
                             cache.setNode(node)
-
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
 
-                            if useCrossDerivatives:
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                            # if useCrossDerivatives:
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
-                            print("node: ", nodeIdentifier, "||", x)
-                            print("n1: ", n1)
-                            print("n2: ", n2)
-                            print("n3: ", n3)
+                            # print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == ")
+                            # print("node: ", nodeIdentifier, "||", x)
+                            # print("n1: ", n1)
+                            # print("n2: ", n2)
+                            # print("n3: ", n3)
+
+                            lNodeIds[n3][n2][n1] = nodeIdentifier
                             nodeIdentifier += 1
 
-                        if n1 == 0 and n2 == 0 and n3 == 2:
-                            # at (n1, n2, n3 + 1/2)
-                            x[0] = (n3 * stepLength + n1 * stepLength)
-                            x[2] = n3 * stepHeight - stepHeight / 2
-                            for n12 in range(elementsCountIn+1):
-                                x[1] = n12 * stepWidth
-                                node = nodes.createNode(nodeIdentifier, nodetemplate)
-                                cache.setNode(node)
+                    x[0] = n1 * stepLength
+                    x[1] = n2 * stepWidth
+                    x[2] = n3 * stepHeight
 
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+                    # moving nodes (n1 = last, n2 = last two, + 1/2 stepheight)
+                    if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > 1):
 
-                                if useCrossDerivatives:
-                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                                    coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                        x[2] = n3 * stepHeight + stepHeight / 2
 
-                                print("node: ", nodeIdentifier, "||", x)
-                                print("n1: ", n1)
-                                print("n2: ", n2)
-                                print("n3: ", n3)
-                                nodeIdentifier = nodeIdentifier + 1
+                        node = nodes.createNode(nodeIdentifier, nodetemplate)
+                        cache.setNode(node)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
 
-                        x[0] = (n3 * stepLength + n1 * stepLength)
+                        # if useCrossDerivatives:
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                        #
+                        # print("if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > elementsCount3 - 2)")
+                        # print("node: ", nodeIdentifier, "||", x)
+                        # print("n1: ", n1)
+                        # print("n2: ", n2)
+                        # print("n3: ", n3)
 
-                        if n1 == elementsCountAlong - update and n3 > 1:
-                            # moving nodes (n1 = last, n2 = last two, + 1/2 stepheight)
-                            x[2] = x[2] + stepHeight*0.5
+                        lNodeIds[n3][n2][n1] = nodeIdentifier
+                        nodeIdentifier += 1
+                        continue
 
+                    if x[0] <= width:
+                        node = nodes.createNode(nodeIdentifier, nodetemplate)
+                        cache.setNode(node)
+
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                        coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+
+                        # if useCrossDerivatives:
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                        #
+                        # print("condition: x[0] <= length")
+                        # print("node: ", nodeIdentifier, "||", x)
+                        # print("n1: ", n1)
+                        # print("n2: ", n2)
+                        # print("n3: ", n3)
+
+                        lNodeIds[n3][n2][n1] = nodeIdentifier
+                        nodeIdentifier += 1
+
+                    # extra nodes for triangle element in the middle without affecting other nodes
+                    # at (n1, n2, n3 + 1/2)
+                    if n1 == elementsCount1 and n2 == elementsCount2-1 and n3 == 1:
+                        x[1] = (elementsCount2-2) * stepWidth
+                        x[2] = n3 * stepHeight + 0.5 * stepHeight
+                        for n12 in range(elementsCount1 + 1):
+                            x[0] = n12 * stepLength
                             node = nodes.createNode(nodeIdentifier, nodetemplate)
                             cache.setNode(node)
 
@@ -204,47 +236,38 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
 
-                            if useCrossDerivatives:
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                            # if useCrossDerivatives:
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                            #
+                            # print("if n1 == elementsCount1-1 and n2 == elementsCount2 and n3 == 1")
+                            # print("node: ", nodeIdentifier, "||", x)
+                            # print("n1: ", n1)
+                            # print("n2: ", n2)
+                            # print("n3: ", n3)
 
-                            print('check node :', nodeIdentifier)
-                            print('update:', update)
-                            nodeIdentifier = nodeIdentifier + 1
-
-                            continue
-
-                        x[1] = n2 * stepWidth
-                        x[2] = n3 * stepHeight
-
-                        if x[0] <= length:
-                            node = nodes.createNode(nodeIdentifier, nodetemplate)
-                            cache.setNode(node)
-
-                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
-                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
-                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
-                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
-                            if useCrossDerivatives:
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                                coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
-
-                            print("node: ", nodeIdentifier, "||", x)
-                            print("n1: ", n1)
-                            print("n2: ", n2)
-                            print("n3: ", n3)
-                            nodeIdentifier = nodeIdentifier + 1
+                            lNodeIds[n3][n2][n1] = nodeIdentifier
+                            nodeIdentifier += 1
             update += 1
 
-
-        #create element
+        # Create element
         elementIdentifier = 1
 
 
         fm.endChange()
         return []
+
+    @classmethod
+    def refineMesh(cls, meshrefinement, options):
+        '''
+        Refine source mesh into separate region, with change of basis.
+        :param meshrefinement: MeshRefinement, which knows source and target region.
+        :param options: Dict containing options. See getDefaultOptions().
+        '''
+        refineElementsCount1 = options['Refine Element 1']
+        refineElementsCount2 = options['Refine Element 2']
+        refineElementsCount3 = options['Refine Element 3']
+
+        meshrefinement.refineAllElementsCubeStandard3d(refineElementsCount1, refineElementsCount2, refineElementsCount3)
