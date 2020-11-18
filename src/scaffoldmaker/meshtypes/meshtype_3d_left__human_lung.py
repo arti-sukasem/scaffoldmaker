0536611ccbab0f684d9b8e0de7dda04b8beed8b5
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2020-11-18 17:43:02 +1300

    update node list, but elements still not working

--- src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
+++ src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
@@ -126,7 +126,7 @@ class MeshType_3d_left_human_lung(Scaffold_base):
 
                     if (n1 == 0 or n1 == elementsCount1) and (n2 == 0 or n2 == elementsCount2):
                         continue
-                    elif (n2 > elementsCount2 - update):
+                    elif (n2 > elementsCount2-update):
                         continue
 
                     # extra nodes for triangle element in the middle without affecting other nodes
@@ -149,15 +149,43 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                             #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
                             #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
-                            print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == 0")
-                            print("node: ", nodeIdentifier, "||", x)
-                            print("n1: ", n12)
-                            print("n2: ", n2)
-                            print("n3: ", n3)
-                            #n12 doesn't work!`
-                            lNodeIds[n3][n2][n1] = nodeIdentifier
+                            # print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == 0")
+                            # print("node: ", nodeIdentifier, "||", x)
+                            # print("n1: ", n12)
+                            # print("n2: ", n2)
+                            # print("n3: ", n3)
+
+                            lNodeIds[n3][n2][n12] = nodeIdentifier
+                            lNodeIds[n3][n2].append(None)
+
                             nodeIdentifier += 1
 
+                        lNodeIds[n3].append([])
+                        # continue the row after
+                        for n13 in range(elementsCount1 + 1):
+                            lNodeIds[n3][n2+1].append(None)
+                            x[0] = n13 * stepLength
+                            x[1] = n2 * stepWidth
+                            node = nodes.createNode(nodeIdentifier, nodetemplate)
+                            cache.setNode(node)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                            coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+
+                            # print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == 0")
+                            # print("node: ", nodeIdentifier, "||", x)
+                            # print("n1: ", n1)
+                            # print("n2: ", n2+1)
+                            # print("n3: ", n3)
+                            lNodeIds[n3][n2+1][n13] = nodeIdentifier
+                            nodeIdentifier += 1
+
+                        nodeIdentifier += elementsCount1 + 1
+
+
+                        # print("break")
+                        break
 
                     x[0] = n1 * stepLength
                     x[1] = n2 * stepWidth
@@ -182,11 +210,11 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                         #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
                         #
 
-                        print("if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > elementsCount3 - 2)")
-                        print("node: ", nodeIdentifier, "||", x)
-                        print("n1: ", n1)
-                        print("n2: ", n2)
-                        print("n3: ", n3)
+                        # print("if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > elementsCount3 - 2)")
+                        # print("node: ", nodeIdentifier, "||", x)
+                        # print("n1: ", n1)
+                        # print("n2: ", n2)
+                        # print("n3: ", n3)
 
                         lNodeIds[n3][n2][n1] = nodeIdentifier
                         nodeIdentifier += 1
@@ -208,23 +236,30 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                         #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
                         #
 
-                        print("condition: x[0] <= length")
-                        print("node: ", nodeIdentifier, "||", x)
-                        print("n1: ", n1)
-                        print("n2: ", n2)
-                        print("n3: ", n3)
+                        # print("condition: x[0] <= width")
+                        # print("node: ", nodeIdentifier, "||", x)
+                        # print("n1: ", n1)
+                        # print("n2: ", n2)
+                        # print("n3: ", n3)
 
                         lNodeIds[n3][n2][n1] = nodeIdentifier
                         nodeIdentifier += 1
 
                     # extra nodes for triangle element in the middle without affecting other nodes
                     # at (n1, n2, n3 + 1/2)
-                    if n1 == elementsCount1 and n2 == elementsCount2-1 and n3 == 1:
+                    k = 0
+                    if n1 == elementsCount1 and n2 == elementsCount2-update and n3 == 2:
+                        lNodeIds[n3].append([])
+                        lNodeIds[n3][n2 + 1] = lNodeIds[n3][n2][:]
+
+                        nodeIdentifier_temp = nodeIdentifier - ((elementsCount1+1) * (n2+1)+1)
                         x[1] = (elementsCount2-2) * stepWidth
-                        x[2] = n3 * stepHeight + 0.5 * stepHeight
+                        x[2] = n3 * stepHeight - 0.5 * stepHeight
+
                         for n12 in range(elementsCount1 + 1):
+
                             x[0] = n12 * stepLength
-                            node = nodes.createNode(nodeIdentifier, nodetemplate)
+                            node = nodes.createNode(nodeIdentifier_temp, nodetemplate)
                             cache.setNode(node)
 
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
@@ -238,15 +273,21 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                             #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
                             #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
-                            print("if n1 == elementsCount1-1 and n2 == elementsCount2 and n3 == 1")
-                            print("node: ", nodeIdentifier, "||", x)
-                            print("n1: ", n1)
-                            print("n2: ", n2)
-                            print("n3: ", n3)
 
-                            lNodeIds[n3][n2][n1] = nodeIdentifier
-                            nodeIdentifier += 1
+                            # print("if n1 == elementsCount1 and n2 == elementsCount2-update and n3 == 2")
+                            # print("nodeIdentifier_temp: ", nodeIdentifier_temp, "||", x)
+                            # print("n1: ", n12)
+                            # print("n2: ", n2)
+                            # print("n3: ", n3)
+                            lNodeIds[n3][n2][n12] = nodeIdentifier_temp
+                            nodeIdentifier_temp += 1
+
+                        continue
+
             update += 1
+
+
+
 
         # ----------------------------------------------------------------------------------------
         #                                   Create element
@@ -271,16 +312,23 @@ class MeshType_3d_left_human_lung(Scaffold_base):
         # eft5 = eftfactory.createEftWedgeCollapseXi3AtXi1Zero()
         # eft6 = eftfactory.createEftWedgeCollapseXi2AtXi3One()
 
-        # elementIdentifier = 1
-        # for e3 in range(elementsCount3):
-        #     for e2 in range(elementsCount2):
-        #         for e1 in range(elementsCount1):
-        #             eft = eftRegular
-        #             nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1+1], lNodeIds[e3][e2 + 1][e1], lNodeIds[e3][e2+1][e1+1],
-        #                 lNodeIds[e3+1][e2][e1], lNodeIds[e3+1][e2][e1+1], lNodeIds[e3+1][e2 + 1][e1], lNodeIds[e3+1][e2+1][e1+1]]
-        #             scalefactors = None
-        #
-        #             print("elementIdentifier: ", elementIdentifier, "|| NodeIdentifiers: ", nodeIdentifiers)
+        elementIdentifier = 1
+        for e3 in range(elementsCount3):
+            for e2 in range(elementsCount2):
+                for e1 in range(elementsCount1):
+                    eft = eftRegular
+                    if (e2 == elementsCount2-1 and e3 == 1):
+                        z = 0
+                    else:
+                        nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1+1], lNodeIds[e3][e2 + 1][e1], lNodeIds[e3][e2+1][e1+1],
+                            lNodeIds[e3+1][e2][e1], lNodeIds[e3+1][e2][e1+1], lNodeIds[e3+1][e2 + 1][e1], lNodeIds[e3+1][e2+1][e1+1]]
+                    scalefactors = None
+
+                    print("lNodeIds: ", lNodeIds[e3][e2][e1], "|| ", e1, e2, e3)
+
+                    print("elementIdentifier: ", elementIdentifier, "|| NodeIdentifiers: ", nodeIdentifiers)
+
+
 
                     # if (e3 < elementsCount3 - 1):
                     #     if (e2 == 0) and (e1 == 0):
@@ -313,7 +361,7 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                     # if scalefactors:
                     #     element.setScaleFactors(eft, scalefactors)
 
-                    # elementIdentifier += 1
+                    elementIdentifier += 1
 
         annotationGroups = []
         fm.endChange()
