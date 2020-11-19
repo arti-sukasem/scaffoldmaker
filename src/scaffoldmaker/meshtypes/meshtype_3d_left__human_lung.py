aed5fdba660f89d061db5d9664a3588785d8a939
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2020-11-19 16:55:15 +1300

    formed elements (except tetrahedral elements), yet the derivatives still need to be sorted out

--- src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
+++ src/scaffoldmaker/meshtypes/meshtype_3d_left__human_lung.py
@@ -4,6 +4,8 @@ Generates a 3-D unit box mesh with variable numbers of elements in 3 directions.
 
 from __future__ import division
 import math
+from scaffoldmaker.annotation.annotationgroup import AnnotationGroup
+from scaffoldmaker.annotation.lung_terms import get_lung_term
 from opencmiss.utils.zinc.field import findOrCreateFieldCoordinates
 from opencmiss.zinc.element import Element, Elementbasis
 from opencmiss.zinc.field import Field
@@ -104,16 +106,14 @@ class MeshType_3d_left_human_lung(Scaffold_base):
         #create nodes
         nodeIdentifier = 1
         x = [0.0, 0.0, 0.0]
-        dx_ds1 = [0.0, 0.0, 0.0]
-        dx_ds2 = [0.0, 0.0, 0.0]
-        dx_ds3 = [0.0, 0.0, 0.0]
-        zero = [0.0, 0.0, 0.0]
+        dx_ds1 = [1.0, 0.0, 0.0]
+        dx_ds2 = [0.0, 1.0, 0.0]
+        dx_ds3 = [0.0, 0.0, 1.0]
         stepLength = length/elementsCount1
         stepWidth = width/elementsCount2
         stepHeight = height/elementsCount3
         lNodeIds = []   # Node index list
         update = 0  # Update the loop to eliminate for the slope
-
 
         for n3 in range(elementsCount3+1):
             lNodeIds.append([])
@@ -123,31 +123,22 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                 x[1] = n2 * stepWidth
                 for n1 in range(elementsCount1+1):
                     lNodeIds[n3][n2].append(None)
-
                     if (n1 == 0 or n1 == elementsCount1) and (n2 == 0 or n2 == elementsCount2):
                         continue
                     elif (n2 > elementsCount2-update):
                         continue
-
                     # extra nodes for triangle element in the middle without affecting other nodes
                     # at (n1 + 1/2, n2, n3)
                     if n2 == elementsCount2-1 and n3 == 1 and n1 == 0:
                         x[1] = n2*stepWidth - stepWidth/2
                         for n12 in range(elementsCount1+1):
                             x[0] = n12 * stepLength
-
                             node = nodes.createNode(nodeIdentifier, nodetemplate)
                             cache.setNode(node)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
-                            # if useCrossDerivatives:
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
 
                             # print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == 0")
                             # print("node: ", nodeIdentifier, "||", x)
@@ -157,10 +148,9 @@ class MeshType_3d_left_human_lung(Scaffold_base):
 
                             lNodeIds[n3][n2][n12] = nodeIdentifier
                             lNodeIds[n3][n2].append(None)
-
                             nodeIdentifier += 1
-
                         lNodeIds[n3].append([])
+
                         # continue the row after
                         for n13 in range(elementsCount1 + 1):
                             lNodeIds[n3][n2+1].append(None)
@@ -172,7 +162,6 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
                             # print("condition: n2 == elementsCount2-1 and n3 == 1 and n1 == 0")
                             # print("node: ", nodeIdentifier, "||", x)
                             # print("n1: ", n1)
@@ -180,11 +169,7 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                             # print("n3: ", n3)
                             lNodeIds[n3][n2+1][n13] = nodeIdentifier
                             nodeIdentifier += 1
-
                         nodeIdentifier += elementsCount1 + 1
-
-
-                        # print("break")
                         break
 
                     x[0] = n1 * stepLength
@@ -193,22 +178,13 @@ class MeshType_3d_left_human_lung(Scaffold_base):
 
                     # moving nodes (n1 = last, n2 = last two, + 1/2 stepheight)
                     if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > 1):
-
                         x[2] = n3 * stepHeight + stepHeight / 2
-
                         node = nodes.createNode(nodeIdentifier, nodetemplate)
                         cache.setNode(node)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
-                        # if useCrossDerivatives:
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
-                        #
 
                         # print("if (n1 > 0 or n1 < elementsCount1) and n2 == 0 and (n3 > elementsCount3 - 2)")
                         # print("node: ", nodeIdentifier, "||", x)
@@ -223,18 +199,10 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                     if x[0] <= width:
                         node = nodes.createNode(nodeIdentifier, nodetemplate)
                         cache.setNode(node)
-
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                         coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
-                        # if useCrossDerivatives:
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                        #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
-                        #
 
                         # print("condition: x[0] <= width")
                         # print("node: ", nodeIdentifier, "||", x)
@@ -251,43 +219,29 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                     if n1 == elementsCount1 and n2 == elementsCount2-update and n3 == 2:
                         lNodeIds[n3].append([])
                         lNodeIds[n3][n2 + 1] = lNodeIds[n3][n2][:]
-
                         nodeIdentifier_temp = nodeIdentifier - ((elementsCount1+1) * (n2+1)+1)
                         x[1] = (elementsCount2-2) * stepWidth
                         x[2] = n3 * stepHeight - 0.5 * stepHeight
 
                         for n12 in range(elementsCount1 + 1):
-
                             x[0] = n12 * stepLength
                             node = nodes.createNode(nodeIdentifier_temp, nodetemplate)
                             cache.setNode(node)
-
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_VALUE, 1, x)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS1, 1, dx_ds1)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS2, 1, dx_ds2)
                             coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D_DS3, 1, dx_ds3)
-
-                            # if useCrossDerivatives:
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS2, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS1DS3, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D2_DS2DS3, 1, zero)
-                            #     coordinates.setNodeParameters(cache, -1, node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
-
 
                             # print("if n1 == elementsCount1 and n2 == elementsCount2-update and n3 == 2")
                             # print("nodeIdentifier_temp: ", nodeIdentifier_temp, "||", x)
                             # print("n1: ", n12)
                             # print("n2: ", n2)
                             # print("n3: ", n3)
+
                             lNodeIds[n3][n2][n12] = nodeIdentifier_temp
                             nodeIdentifier_temp += 1
-
                         continue
-
             update += 1
-
-
-
 
         # ----------------------------------------------------------------------------------------
         #                                   Create element
@@ -304,13 +258,23 @@ class MeshType_3d_left_human_lung(Scaffold_base):
         elementtemplateCustom = mesh.createElementtemplate()
         elementtemplateCustom.setElementShapeType(Element.SHAPE_TYPE_CUBE)
 
+        lungGroup = AnnotationGroup(region, get_lung_term("lung"))
+        leftLungGroup = AnnotationGroup(region, get_lung_term("left lung"))
+        lowerLobeLeftLungGroup = AnnotationGroup(region, get_lung_term("lower lobe of left lung"))
+        annotationGroups = [leftLungGroup, lowerLobeLeftLungGroup, lungGroup]
+
+        lungMeshGroup = lungGroup.getMeshGroup(mesh)
+        leftLungMeshGroup = leftLungGroup.getMeshGroup(mesh)
+        lowerLobeLeftLungMeshGroup = lowerLobeLeftLungGroup.getMeshGroup(mesh)
+
         eft1 = eftfactory.createEftWedgeCollapseXi1AtXi2Zero()
         eft2 = eftfactory.createEftWedgeCollapseXi1AtXi2One()
-        eft3 = eftfactory.createEftWedgeCollapseXi2AtXi3One()
-        # eft3 = eftfactory.createEftWedgeCollapseXi3AtXi2Zero()
-        # eft4 = eftfactory.createEftWedgeCollapseXi3AtXi2One()
-        # eft5 = eftfactory.createEftWedgeCollapseXi3AtXi1Zero()
-        # eft6 = eftfactory.createEftWedgeCollapseXi2AtXi3One()
+        eft3 = eftfactory.createEftWedgeCollapseXi2RightAtXi3One()
+        eft4 = eftfactory.createEftWedgeCollapseXi2LeftAtXi3One()
+        eft5 = eftfactory.createEftWedgeCollapseXi1AtXi3One()
+        eft6 = eftfactory.createEftTetrahedronCollapseXi1Xi2AtXi3OneXi1AtXi2Zero()
+        eft7 = eftfactory.createEftTetrahedronCollapseXi1Xi2AtXi3OneXi1AtXi2One()
+
 
         elementIdentifier = 1
         for e3 in range(elementsCount3):
@@ -318,53 +282,51 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                 for e1 in range(elementsCount1):
                     eft = eftRegular
                     if e2 == elementsCount2-2 and e3 == 1:
+                        # Wedge in the middle of the lower lobe
                         nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1 + 1], lNodeIds[e3][e2 + 1][e1],
                                            lNodeIds[e3][e2 + 1][e1 + 1], lNodeIds[e3 + 1][e2][e1], lNodeIds[e3 + 1][e2][e1 + 1]]
+                        eft = eft4
+                        # print("option: 1", elementIdentifier)
                     elif e2 == elementsCount2-1 and e3 == 1:
+                        # Element just above the middle wedge
                         nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1 + 1], lNodeIds[e3][e2 + 1][e1],
-                                           lNodeIds[e3][e2 + 1][e1 + 1],
-                                           lNodeIds[e3 + 1][e2][e1], lNodeIds[e3 + 1][e2][e1 + 1],
-                                           lNodeIds[e3 + 1][e2 - 1][e1], lNodeIds[e3 + 1][e2 - 1][e1 + 1]]
+                                           lNodeIds[e3][e2 + 1][e1 + 1], lNodeIds[e3 + 1][e2 - 1][e1],
+                                           lNodeIds[e3 + 1][e2 - 1][e1 + 1],
+                                           lNodeIds[e3 + 1][e2][e1], lNodeIds[e3 + 1][e2][e1 + 1]
+                                           ]
+                        # print("option: 2", elementIdentifier)
                     elif e2 == elementsCount2 - update + 1 and e3 == elementsCount3-1:
+                        # Element just above the middle wedge in the next column
                         nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1 + 1], lNodeIds[e3][e2 + 1][e1],
                                            lNodeIds[e3][e2 + 1][e1 + 1],
                                            lNodeIds[e3 + 1][e2][e1], lNodeIds[e3 + 1][e2][e1 + 1],
                                            lNodeIds[e3][e2 + 2][e1], lNodeIds[e3][e2 + 2][e1 + 1]]
+                        # print("option: 3", elementIdentifier)
                     else:
+                        # the remaining elements
                         nodeIdentifiers = [lNodeIds[e3][e2][e1], lNodeIds[e3][e2][e1+1], lNodeIds[e3][e2 + 1][e1], lNodeIds[e3][e2+1][e1+1],
                             lNodeIds[e3+1][e2][e1], lNodeIds[e3+1][e2][e1+1], lNodeIds[e3+1][e2 + 1][e1], lNodeIds[e3+1][e2+1][e1+1]]
+                        # print("option: 4", elementIdentifier)
+
                     scalefactors = None
 
-                    #Discard all the unused elements
-                    if nodeIdentifiers.count(None) > 3:
+                    # Identifying element types
+                    if nodeIdentifiers.count(None) > 2:
+                        # Skip extra node sets
                         continue
+                    elif nodeIdentifiers.count(None) == 2:
+                        # Wedge element type
+                        eft = eft1
+                    elif nodeIdentifiers.count(None) == 1:
+                        # Tetrahedral element type
+                        continue
+
+                    # Remove None from the list
+                    nodeIdentifiers = list(filter(None.__ne__, nodeIdentifiers))
 
                     print("elementIdentifier: ", elementIdentifier, "|| NodeIdentifiers: ", nodeIdentifiers)
 
-
-                    if (e3 < elementsCount3 - 1):
-                        if (e2 == 0) and (e1 == 0):
-                            # Back wedge elements
-                            nodeIdentifiers.pop(4)
-                            nodeIdentifiers.pop(0)
-                            eft = eft1
-                            scalefactors = [-1.0]
-                        elif (e2 == (elementsCount2 - 1)) and (e1 == 0):
-                            # Front wedge elements
-                            nodeIdentifiers.pop(6)
-                            nodeIdentifiers.pop(2)
-                            eft = eft2
-                    else:
-                        if ((e2 == 0) or (e2 == elementsCount2 - 1)) and (e1 == 0):
-                            # Top tetrahedron elements
-                            continue
-                        elif (e2 == 0) and (e1 == 1):
-                            # Top back wedge elements
-                            nodeIdentifiers.pop(4)
-                            nodeIdentifiers.pop(3)
-                            eft = eft3
-
-
+                    # Forming an element and annotating the elements
                     if eft is eftRegular:
                         element = mesh.createElement(elementIdentifier, elementtemplateRegular)
                     else:
@@ -373,10 +335,12 @@ class MeshType_3d_left_human_lung(Scaffold_base):
                     element.setNodesByIdentifier(eft, nodeIdentifiers)
                     if scalefactors:
                         element.setScaleFactors(eft, scalefactors)
-
                     elementIdentifier += 1
+                    leftLungMeshGroup.addElement(element)
+                    lungMeshGroup.addElement(element)
+                    lowerLobeLeftLungMeshGroup.addElement(element)
 
-        annotationGroups = []
+
         fm.endChange()
         return annotationGroups
 
